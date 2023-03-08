361221bd3d79d5ae436855c2110207ffd31f7175
wassai625 <98636488+wassai625@users.noreply.github.com>
2023-03-08 16:14:42 +0900

    add_answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_061827) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_061934) do
+  create_table "answers", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.integer "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "name"
     t.string "title"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_061827) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
