5fe6f75dc92edd4774db22e819a554c613a71377
wassai625 <98636488+wassai625@users.noreply.github.com>
2023-03-08 15:02:46 +0900

    add_edit_destroy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,15 +24,22 @@ class QuestionsController < ApplicationController
     end
     # 質問の編集
     def edit
-
+        @question = Question.find(params[:id])
     end
     # 質問の更新
     def update
-
+        @question = Question.find(params[:id])
+        if @question.update(question_params)
+            redirect_to @question
+        else
+            render 'edit', status: :unprocessable_entity
+        end
     end
     # 質問の削除
     def destroy
-
+        @question = Question.find(params[:id])
+        @question.destroy
+        redirect_to questions_path
     end
 
     private
